58d52a0adb2043d8e08c7613b48a5d7f1ad72ce2
Shiori Dev <shiori@example.com>
2026-02-23 09:56:43 +0530

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: host || true, // listen on all addresses to allow testing from mobile browser
+    hmr: host
+      ? {
+        protocol: "ws",
+        host,
+        port: 5174,
+      }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"], // tell vite to ignore watching `src-tauri`
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

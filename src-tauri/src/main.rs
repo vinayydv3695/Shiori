d6780a0fdf482be9694e3d2d6242cbe17b24b50d
Shiori Dev <shiori@example.com>
2026-03-05 11:34:22 +0530

    fix: linux render issues

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -37,6 +37,11 @@ pub struct MetadataState {
 }
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
+
     env_logger::init();
 
     tauri::Builder::default()

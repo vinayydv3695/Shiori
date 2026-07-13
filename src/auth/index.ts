20a441a915c4a4aa8be4a2cfa47f9259208ddba5
vinayydv3695 <vinayydv343@gmail.com>
2026-07-13 09:16:28 +0530

    Restore Android AniList provider to fix Turnstile validation error

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,6 +1,8 @@
 import { isAndroid } from '@/lib/tauri';
 import { AniListAuthProvider } from './AniListProvider';
 import { AniListDesktopProvider } from './desktop/AniListDesktopProvider';
-export const anilistAuth: AniListAuthProvider = new AniListDesktopProvider();
+import { AniListAndroidProvider } from './android/AniListAndroidProvider';
+
+export const anilistAuth: AniListAuthProvider = isAndroid() ? new AniListAndroidProvider() : new AniListDesktopProvider();
 
 export * from './AniListProvider';
